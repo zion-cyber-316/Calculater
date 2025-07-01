4bfc7e3b2af2dea320c17c2ac67ddc58b7b879e7
Gaurav mehra <gaurav9759434559@gmail.com>
2025-07-01 15:42:28 +0530

    Add files via upload
    calculator file

---
+++ script.js
@@ -0,0 +1,17 @@
+   const display = document.getElementById('display');
+
+    function appendValue(value) {
+      display.value += value;
+    }
+
+    function clearDisplay() {
+      display.value = '';
+    }
+
+    function calculate() {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = 'Error';
+      }
+    }
